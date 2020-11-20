f1b69419cfceb21cc0f072a12094c83fac9a5095
Brian Pease <bspease@calpoly.edu>
2020-11-19 17:41:40 -0800

    stubs

--- libDisk.c
+++ libDisk.c
@@ -7,3 +7,18 @@ int openDisk(char *filename, int nBytes) {
     int num_blocks = nBytes % BLOCKSIZE;
 
 }
+
+int closeDisk(int disk)
+{
+    
+}
+
+int readBlock(int disk, int bNum, void *block)
+{
+
+}
+
+int writeBlock(int disk, int bNum, void *block)
+{
+
+}
