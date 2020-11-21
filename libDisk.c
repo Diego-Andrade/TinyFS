07af56de398a1d2669d4f0f5d5fa0210335ac3b0
Diego-Andrade <bets636@gmail.com>
2020-11-21 12:29:34 -0800

    Adding link list to openDisk()

--- libDisk.c
+++ libDisk.c
@@ -5,37 +5,43 @@
 #include "libDisk.h"
 #include "linkedList.h"
 
-static LList *FDTable;
-static disk_count = 0;
+static LList *mountTable;
+static int disk_count = 0;
 
 int openDisk(char *filename, int nBytes) {
     if (nBytes < BLOCKSIZE) 
         return BYTES_SMALLER_THAN_BLOCKSIZE;
 
-    int disk_num = getDiskNum(filename);
+    int disk_num = getDiskNum(mountTable, filename);
 
     if (nBytes == 0 && disk_num != -1) 
         return disk_num;
 
     int num_blocks = nBytes % BLOCKSIZE;
-    disk_num = addDisk(filename, num_blocks);
+    disk_num = ++disk_count;
+    
+    if (mountTable == NULL)
+        mountTable = createLinkedList();
+        
+    registerDisk(mountTable, disk_num, filename, num_blocks);
 
     FILE* file = fopen(filename, "wb+");
-    for (int i = 0; i < BLOCKSIZE; i++)
+    for (int i = 0; i < num_blocks * BLOCKSIZE; i++)
         fprintf(file, "0");
     fclose(file);
+
+    return disk_num;
 }
 
-int closeDisk(int disk)
-{
-    
+int closeDisk(int disk) {
+    removeNode(mountTable, disk);
 }
 
 int readBlock(int disk, int bNum, void *block)
 {
     off_t offset = bNum*BLOCKSIZE;
     int fd;
-    Node *entry = getNode(FDTable, disk);
+    Node *entry = getNode(mountTable, disk);
 
     fd = open(getDiskName(disk));
     if (lseek(fd, offset, SEEK_SET) == -1)    // Sets File offset to offset bytes
@@ -58,7 +64,7 @@ int writeBlock(int disk, int bNum, void *block)
     int fd;
     Node *entry;
 
-    entry = getNode(FDTable, disk);
+    entry = getNode(mountTable, disk);
     if (bNum >= entry->numBlocks)
         return OUT_OF_BOUNDS;
     offset = bNum*BLOCKSIZE;
