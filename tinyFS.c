b4344b9fdb1df7c873d44ff6e8d720649cc5bd71
Brian Pease <bspease@calpoly.edu>
2020-12-01 23:53:11 -0800

    bloop My test file

--- tinyFS.c
+++ tinyFS.c
@@ -2,10 +2,45 @@
 #include <stdio.h>
 
 #include "libTinyFS.h"
+#include "tinyFS.h"
+#include "tinyFS_errno.h"
 
+#define A_SIZE 912  //4 blocks 3 full + 150
+#define B_SIZE 245  //1 Block   245 
 int main() {
     /* Our demo code? */
-    tfs_mkfs(DEFAULT_DISK_NAME, DEFAULT_DISK_SIZE);
+    int fdA;
+    int fdB;
+    char bufferA[A_SIZE];
+    char bufferB[B_SIZE];
+    int i;
+    for (i = 0; i< A_SIZE; i++)
+    {
+        bufferA[i] = 'A';
+    }
+    for (i = 0; i< B_SIZE; i++)
+    {
+        bufferB[i] = 'B';
+    }
+
+    if (tfs_mkfs(DEFAULT_DISK_NAME, BLOCKSIZE*9) < 0)
+        return -10;
+    if (tfs_mount(DEFAULT_DISK_NAME) < 0)
+        return -10;
+    if ((fdA = tfs_openFile("AFile")) < 0)
+        return -10;
+    if ((fdB = tfs_openFile("BFile")) < 0)
+        return -10;
+    if (tfs_writeFile(fdA, bufferA, A_SIZE) < 0)
+        return -10;
+    if (tfs_writeFile(fdB, bufferB, B_SIZE) < 0)
+        return -10;
+    if (tfs_unmount() < 0)
+        return -10;
+    return 0;
+
+    // if (tfs_closeFile(fd) < 0)
+    //     return -10;
 
 }
 
